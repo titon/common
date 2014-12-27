6e0eeef59888de01bbd580aee90116a058ab5212
Miles Johnson <miles.johnson@hyfn.com>
2014-12-27 00:12:50 -0800

    Separated validation into an interface. Moved to the common package.

---
+++ Exception/InvalidValidationRuleException.hh
@@ -0,0 +1,17 @@
+<?hh // strict
+/**
+ * @copyright   2010-2015, The Titon Project
+ * @license     http://opensource.org/licenses/bsd-license.php
+ * @link        http://titon.io
+ */
+
+namespace Titon\Common\Exception;
+
+/**
+ * Exception thrown for an invalid validation rule.
+ *
+ * @package Titon\Utility\Exception
+ */
+class InvalidValidationRuleException extends \LogicException {
+
+}
